9a01524caaf7080c7f074c73d67240c8f582dbf7
Fabio Soares Schmidt <fabio@improve.inf.br>
2013-01-05 04:15:20 -0800

    Create disclaimer-excecao.sh

---
+++ Disclaimer/disclaimer-excecao.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# Fabio S. Schmidt <fabio@improve.inf.br>
+# 05/Jan/2013
+# Contempla exceções de remetentes que não terão o disclaimer inserido
+# Inserir os remetentes para exceção no arquivo definido pela variavel EXCESSOES_REMETENTES
+
+# Localize these.
+INSPECT_DIR=/var/spool/filter
+SENDMAIL=/usr/sbin/sendmail
+
+#INCLUIDO PARA INCLUIR O CONTEUDO SOMENTE EM MENSAGENS ENVIADAS
+EXCECOES_REMETENTES=/etc/postfix/excecoes_remetente
+
+# Exit codes from <sysexits.h>
+EX_TEMPFAIL=75
+EX_UNAVAILABLE=69
+
+# Clean up when done or when aborting.
+trap "rm -f in.$$" 0 1 2 3 15
+
+# Start processing.
+cd $INSPECT_DIR || { echo $INSPECT_DIR does not exist; exit
+$EX_TEMPFAIL; }
+
+cat >in.$$ || { echo Cannot save mail to file; exit $EX_TEMPFAIL; }
+
+#FAZ A PESQUISA DO REMETENTE PARA A EXCECAO
+from_address=`grep -m 1 "From:" in.$$ | cut -d " " -f 2 | cut -d " " -f 1`
+
+
+if [ `grep -wi ^${from_address}$ ${EXCECOES_REMETENTES}` ]; then
+
+$SENDMAIL -oi "$@" <in.$$
+exit $?
+
+else
+
+/usr/bin/altermime --input=in.$$ \
+                   --disclaimer=/etc/postfix/disclaimer.txt \
+                   --disclaimer-html=/etc/postfix/disclaimer.html \
+                   --xheader="X-Copyrighted-Material: Colocar o site da empresa aqui http://www.company.com/privacy.htm" || \
+                     { echo Message content rejected; exit $EX_UNAVAILABLE; }
+$SENDMAIL -oi "$@" <in.$$
+exit $?
+
+fi
